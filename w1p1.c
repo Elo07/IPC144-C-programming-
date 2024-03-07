2a2d205e6857bc2be0db6842859b98165db8967b
Larry Okuonghae <elokuonghae@myseneca.ca>
2024-03-07 09:59:18 -0500

    workshop 1

---
+++ w1p1.c
@@ -0,0 +1,20 @@
+/*
+ *****************************************************************************
+ <assessment name example: Workshop - #1 (Part-1)>
+   Full Name :Ehimwenma Larry Okuonghae
+   Student ID#:145203238
+   Email :elokuonghae@myseneca.ca
+   Section :NGG
+   Authenticity Declaration:
+   I declare this submission is the result of my own work and has not been
+   shared with any other student or 3rd party content provider. This submitted
+   piece of work is entirely of my own creation.
+   *****************************************************************************
+ */
+#include <stdio.h>
+
+int main(void){
+    
+    printf("Workshop 1 Part-1\n=================\n\nI'm displaying this using the 'printf' stdio\n(standard input output) library function!\n\nDear teacher,\n\n  I promise I will work hard from this day onward.\n  I acknowledge that practice is extremely important,\n  so I will do all workshops, quizzes, and assignments.\n\nSincerely,\n\nEhimwenma Larry Okuonghae\nStudent ID# 145203238\n");
+    return 0;
+}
